548b7435a87630f917d6c2eb2e4240ed462e90d3
amitjait <amit.rimit591@gmail.com>
2023-03-18 18:25:12 +0530

    Updating

--- src/Components/GenreFilter.js
+++ src/Components/GenreFilter.js
@@ -3,8 +3,8 @@ import FilterButton from "./FilterButton"
 function GenreFilter(){
     
     return (
-        <div className="container-fluid bg-body-secondary rounded justify-content-center p-2" style={{"width": "58%", "height":""}}>
-            <h2 className="ms-3">Filter by Genre</h2>
+        <div className="container-fluid bg-body-secondary rounded justify-content-center p-2" style={{"width": "60%", "height":""}}>
+            <h2 className="ms-5">Filter by Genre</h2>
             <FilterButton />
         </div>  
     )
--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -2,10 +2,10 @@
 function MoviesList({title, year, genre}){
 
     return (
-        <div className="container-md d-flex" style={{"width": "60%", "height":"35px"}}>
-            <div className="container-md border" style={{"width":"60%", "height" : "35px"}}>{title}</div>
-            <div className="container-md border" style={{"width":"25%", "height" : "35px"}}>{genre}</div>
-            <div className="container-md border" style={{"width":"20%", "height" : "35px"}}>{year}</div>
+        <div className="container-md d-flex" style={{"width": "62%", "height":"fit-content"}}>
+            <div className="container-md border text-wrap p-1" style={{"width":"60%", "height" : "100%"}}>{title}</div>
+            <div className="container-md border p-1" style={{"width":"25%", "height" : "100%"}}>{genre}</div>
+            <div className="container-md border p-1" style={{"width":"20%", "height" : "100%"}}>{year}</div>
         </div>
     )
 }
