aeec1e5634534b5730e774037b191e5f51b3c681
amitjait <amit.rimit591@gmail.com>
2023-03-18 18:27:20 +0530

    Updating

--- src/Components/MovieHeader.js
+++ src/Components/MovieHeader.js
@@ -2,9 +2,9 @@
 function MovieHeader(){
     return (
         <div className="container-md d-flex " style={{"width": "62%", "height":"fit-content"}}>
-            <div className="container-md border bg-secondary-subtle p-2" style={{"width":"60%", "height" : "35px"}}><b>Title</b></div>
-            <div className="container-md border bg-secondary-subtle p-2" style={{"width":"25%", "height" : "35px"}}><b>Genre</b></div>
-            <div className="container-md border bg-secondary-subtle p-2" style={{"width":"20%", "height" : "35px"}}><b>year</b></div>
+            <div className="container-md border bg-secondary-subtle p-1" style={{"width":"60%", "height" : "35px"}}><b>Title</b></div>
+            <div className="container-md border bg-secondary-subtle p-1" style={{"width":"25%", "height" : "35px"}}><b>Genre</b></div>
+            <div className="container-md border bg-secondary-subtle p-1" style={{"width":"20%", "height" : "35px"}}><b>year</b></div>
         </div>
     )
 }
